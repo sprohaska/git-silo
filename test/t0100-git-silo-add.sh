22f38a050c0540e92fd63ca167e16e19c8f65e48
Steffen Prohaska <prohaska@zib.de>
2013-02-22 17:40:11 +0100

    add, checkout: Handle filename with spaces

--- test/t0100-git-silo-add.sh
+++ test/t0100-git-silo-add.sh
@@ -4,13 +4,41 @@ test_description="git-silo (basic)"
 
 . ./sharness/sharness.sh
 
+blobSize() {
+    git ls-tree -rl HEAD -- "$1" |
+    cut -f 1 |
+    sed -e 's/  */ /g' |
+    cut -d ' ' -f 4
+}
+
 test_expect_success \
 "'git-silo add' should handle paths with spaces." \
 '
     git init &&
+    touch .gitignore &&
+    git add .gitignore &&
+    git commit -m "initial commit" &&
     git-silo init &&
-    touch "a a" &&
-    git-silo add "a a"
+    echo a >"a a" &&
+    git-silo add "a a" &&
+    git commit -m "Add a a" &&
+    ( test $(blobSize "a a") -eq 41 || ( echo "Wrong blob size." && false ) )
+'
+
+test_expect_success \
+"'git checkout' of silo content should handle paths with spaces." \
+'
+    rm "a a" &&
+    git checkout "a a" &&
+    test -e "a a"
+'
+
+test_expect_success \
+"'git-silo checkout' should handle paths with spaces." \
+'
+    rm "a a" &&
+    git-silo checkout "a a" &&
+    test -e "a a"
 '
 
 test_done
