7f8972f482af2b97799f65dc0ef6bb73499622a8
Steffen Prohaska <prohaska@zib.de>
2013-02-22 11:00:24 +0100

    t1000: test gc with two branches

--- test/t1000-git-silo-gc.sh
+++ test/t1000-git-silo-gc.sh
@@ -8,21 +8,32 @@ test_expect_success \
 "Setup" \
 "
     git init &&
+    touch .gitignore &&
+    git add .gitignore &&
+    git commit -m 'initial commit' &&
     git-silo init
     echo a >a &&
     ( openssl sha1 a | cut -d ' ' -f 2 > a.sha1 ) &&
     echo b >b &&
     ( openssl sha1 b | cut -d ' ' -f 2 > b.sha1 ) &&
-    ( cat a.sha1 b.sha1 | sort -u >ab.sha1 )
+    echo c >c &&
+    ( openssl sha1 c | cut -d ' ' -f 2 > c.sha1 ) &&
+    ( cat a.sha1 b.sha1 | sort -u >ab.sha1 ) &&
+    ( cat b.sha1 c.sha1 | sort -u >bc.sha1 ) &&
+    ( cat a.sha1 b.sha1 c.sha1 | sort -u >abc.sha1 )
 "
 
 test_expect_success \
 "'git-silo add' will add objects to silo store." \
 "
+    git checkout -b tmp &&
+    git-silo add c &&
+    git commit -m 'Add c' &&
+    git checkout master &&
     git-silo add a b &&
     git commit -m 'Add a, b' &&
     ( cd .git/silo/objects && find * -type f | sed -e 's@/@@' ) >actual &&
-    test_cmp ab.sha1 actual
+    test_cmp abc.sha1 actual
 "
 
 test_expect_success \
@@ -32,7 +43,7 @@ test_expect_success \
     git commit -m 'Remove a' &&
     git silo gc &&
     ( cd .git/silo/objects && find * -type f | sed -e 's@/@@' ) >actual &&
-    test_cmp b.sha1 actual
+    test_cmp bc.sha1 actual
 "
 
 test_done
