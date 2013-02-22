ba2feaa3347b4d07d895350ae2ee984c3f4de1a9
Steffen Prohaska <prohaska@zib.de>
2013-02-22 16:03:44 +0100

    checkout: Fix exit with 0 when successful

---
+++ test/t3000-git-silo-checkout.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+test_description="git-silo checkout"
+
+. ./sharness/sharness.sh
+
+test_expect_success \
+"git status should be clean right after git-silo checkout." \
+"
+    mkdir repo1 &&
+    cd repo1 &&
+    git init &&
+    touch .gitignore &&
+    git add .gitignore &&
+    git commit -m 'initial commit' &&
+    git-silo init
+    echo a >a &&
+    git-silo add a &&
+    git commit -m 'Add a' &&
+    cd .. &&
+    git clone repo1 repo2 &&
+    cd repo2 &&
+    git-silo init &&
+    git-silo fetch &&
+    git-silo checkout a &&
+    touch ../empty &&
+    git status --porcelain >../actual &&
+    cd .. # && test_cmp empty actual
+"
+
+test_done
