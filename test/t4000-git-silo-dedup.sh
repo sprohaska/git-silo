6c95b3156d1fa3a4aa8f0baab66e95a12dc9a3ad
Steffen Prohaska <prohaska@zib.de>
2013-03-28 18:41:21 +0100

    dedup: Add command to hard link objects from repos

---
+++ test/t4000-git-silo-dedup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+test_description="git-silo dedup"
+
+. ./sharness/sharness.sh
+
+linkCount() {
+    ls -l $1 | sed -e 's/  */ /' | cut -d ' ' -f 2
+}
+
+test_expect_success \
+"git dedup should create hardlinks between two repositories" \
+'
+    mkdir repo1 &&
+    cd repo1 &&
+    git init &&
+    touch .gitignore &&
+    git add .gitignore &&
+    git commit -m "initial commit" &&
+    git-silo init
+    echo a >a &&
+    git-silo add a &&
+    git commit -m "Add a" &&
+    cd .. &&
+    git clone repo1 repo2 &&
+    cd repo2 &&
+    git-silo init &&
+    git-silo fetch &&
+    git-silo checkout . &&
+    ( test $(linkCount a) -eq 2 || ( echo "Wrong link count." && false ) ) &&
+    git-silo dedup ../repo1 . &&
+    rm -r a &&
+    git-silo checkout . &&
+    ( test $(linkCount a) -eq 4 || ( echo "Wrong link count." && false ) )
+'
+
+test_done
