98357500798267014beb8050d33ba366d90ed957
Steffen Prohaska <prohaska@zib.de>
2013-06-08 12:30:21 +0200

    runtests: Report total success or failure

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 cd $(dirname "$0")
+err=
 for t in t????-*.t; do
     echo
     echo "# $t"
-    ./$t
+    ./$t || err=t
 done
+
+echo
+if test $err; then
+    echo "FAIL (some tests failed; see errors above)"
+else
+    echo "OK (all tests passed)"
+fi
