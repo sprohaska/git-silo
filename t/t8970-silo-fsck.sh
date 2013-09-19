13d85badd1de901111a3b35de6377c5a3d7dcfdd
Steffen Prohaska <prohaska@zib.de>
2013-09-19 14:15:02 +0200

    fsck: Handle empty repo

--- t/t8970-silo-fsck.sh
+++ t/t8970-silo-fsck.sh
@@ -6,40 +6,35 @@ Test "silo fsck" to verify consistency of silo store.
 
 . ./lib-silo.sh
 
-test_expect_success \
-"setup user" \
-'
-    setup_user
-'
-
-test_expect_success \
-"Setup" \
-"
+test_expect_success 'setup (empty repo)' '
+    setup_user &&
     git init &&
     touch .gitignore &&
     git add .gitignore &&
-    git commit -m 'initial commit' &&
+    git commit -m "initial commit" &&
     git silo init
-    echo a >a &&
-    git silo add a &&
-    git commit -m 'Add a'
-"
-
-test_expect_success \
-'git silo fsck should exit with zero when repo ok.' \
 '
+
+test_expect_success 'silo fsck should handle empty repo' '
     git silo fsck
 '
 
-test_expect_success \
-'git silo fsck should print ok when repo ok.' \
+test_expect_success 'setup (add files)' '
+    echo a >a &&
+    git silo add a &&
+    git commit -m "Add a"
 '
+
+test_expect_success 'git silo fsck should exit with zero when repo ok.' '
+    git silo fsck
+'
+
+test_expect_success 'git silo fsck should print ok when repo ok.' '
     git silo fsck | grep -q ok
 '
 
 test_expect_success \
-'git silo fsck should exit with nonzero when repo corrupted.' \
-'
+'git silo fsck should exit with nonzero when repo corrupted.' '
     chmod u+w a &&
     echo "corrupted" >>a &&
     ! git silo fsck
