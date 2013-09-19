bde27ed5ea645104591f6f14aa9c27c1ee6c9a7e
Steffen Prohaska <prohaska@zib.de>
2013-09-19 13:35:28 +0200

    pack, unpack: Add support for manually managing 7z packs
    'pack' creates 7z packs from loose objects.  With '--remove', loose
    objects are removed.  Loose objects can be recreated using 'unpack'.
    
    The maximum pack size is controlled by silo.packSizeLimit.  Size
    suffixes k, m, g are supported in addition to K, M, G.
    
    LZMA2 is used for compression, because it scales to multiple threads
    during compression.

---
+++ t/t8965-silo-pack.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+test_description='
+Test packing (WIP)
+'
+
+. ./lib-silo.sh
+
+numObjects() {
+    find .git/silo/objects -type f |
+    wc -l |
+    sed -e 's/ *//g'
+}
+
+assertNumObjects() {
+    [ $(numObjects) == $1 ]
+}
+
+test_expect_success 'setup' '
+    setup_user &&
+    setup_repo repo &&
+    ( cd repo && git config silo.packSizeLimit 8K )
+'
+
+test_expect_success 'pack should handle empty silo' '
+    (
+        cd repo &&
+        git silo pack
+    )
+'
+
+test_expect_success 'setup files (1..5)' '
+    (
+        cd repo &&
+        for i in $(seq 1 5); do
+            printf "%${i}024d" $i >$i &&
+            git silo add $i ||
+            error "failed to setup file $i."
+        done &&
+        git commit -m "add files"
+    )
+'
+
+test_expect_success 'pack' '
+    (
+        cd repo &&
+        git silo pack
+    )
+'
+
+test_expect_success "'pack --keep' should keep loose objects." '
+    (
+        cd repo &&
+        git silo pack --keep &&
+        assertNumObjects 5
+    )
+'
+
+test_expect_success "'pack --remove' should remove loose objects." '
+    (
+        cd repo &&
+        git silo pack --remove &&
+        assertNumObjects 0
+    )
+'
+
+test_expect_success "'unpack 1' should create one loose object." '
+    (
+        cd repo &&
+        git silo unpack 1 &&
+        assertNumObjects 1
+    )
+'
+
+test_expect_success "'unpack' should create all 5 loose object." '
+    (
+        cd repo &&
+        git silo unpack &&
+        assertNumObjects 5
+    )
+'
+
+test_expect_success 'setup files (6..10)' '
+    (
+        cd repo &&
+        for i in $(seq 6 10); do
+            printf "%${i}024d" $i >$i &&
+            git silo add $i ||
+            error "failed to setup file $i."
+        done &&
+        git commit -m "add files"
+    )
+'
+
+test_expect_success 'pack' '
+    (
+        cd repo &&
+        git silo pack
+    )
+'
+
+test_expect_success 'setup files (11.99)' '
+    (
+        cd repo &&
+        for i in $(seq 11 99); do
+            printf "%3${i}0d" $i >$i &&
+            git silo add $i ||
+            error "failed to setup file $i."
+        done &&
+        git commit -m "add files"
+    )
+'
+
+test_expect_success "'pack --remove' should keep 2 (large) loose objects." '
+    (
+        cd repo &&
+        git silo pack --remove &&
+        assertNumObjects 2
+    )
+'
+
+test_expect_success "'unpack' should create all 99 loose object." '
+    (
+        cd repo &&
+        git silo unpack &&
+        assertNumObjects 99
+    )
+'
+
+test_done
