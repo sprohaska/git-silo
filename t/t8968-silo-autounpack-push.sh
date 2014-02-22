223a02e096278f8a859b296909432ccf0a0900bd
Steffen Prohaska <prohaska@zib.de>
2014-02-22 12:50:48 +0100

    push: Autounpack

---
+++ t/t8968-silo-autounpack-push.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+test_description='
+Test automatic unpack
+'
+
+. ./lib-silo.sh
+
+ssh localhost true 2>/dev/null && test_set_prereq LOCALHOST
+
+test_expect_success "setup" '
+    setup_user &&
+    setup_file first &&
+    setup_repo repo1
+'
+
+test_expect_success "push (cp) should autounpack." '
+    git clone repo1 cpclone && (
+        cd cpclone &&
+        git silo init
+    ) &&
+    setup_add_file cpclone first && (
+        cd cpclone &&
+        git silo pack --all --prune &&
+        git silo push -- .
+    ) &&
+    assertRepoHasSiloObject repo1 first
+'
+
+test_expect_success "cleanup" '
+    rm -f repo1/.git/silo/objects/*/*
+    rmdir repo1/.git/silo/objects/*
+'
+
+test_expect_success "push (sshcat) should autounpack." '
+    setup_clone_ssh repo1 sshclone && (
+        cd sshclone &&
+        git silo init
+    ) &&
+    setup_add_file sshclone first && (
+        cd sshclone &&
+        git silo pack --all --prune &&
+        git silo push -- .
+    ) &&
+    assertRepoHasSiloObject repo1 first
+'
+
+test_done
