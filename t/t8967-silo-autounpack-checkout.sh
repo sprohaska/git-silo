d934bd77572618409906e3cd2025ac53d6aed805
Steffen Prohaska <prohaska@zib.de>
2014-02-22 11:13:06 +0100

    checkout: try to autounpack missing objects

---
+++ t/t8967-silo-autounpack-checkout.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+test_description='
+Test automatic unpack
+'
+
+. ./lib-silo.sh
+
+test_expect_success "setup" '
+    setup_user &&
+    setup_file first &&
+    setup_repo repo &&
+    setup_add_file repo first
+'
+
+test_expect_success "setup (prune)" '(
+    cd repo &&
+    git silo pack --prune --all
+)'
+
+test_expect_success "checkout should autounpack" '(
+    cd repo &&
+    rm -f first &&
+    git silo checkout first
+)'
+
+test_done
