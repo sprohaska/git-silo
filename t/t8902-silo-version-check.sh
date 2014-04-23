ced4f9d4e30227b8d5efab4244b578da09804bc2
Steffen Prohaska <prohaska@zib.de>
2014-04-23 12:54:43 +0200

    git-silo: Fix version check to accept git 2.x

--- t/t8902-silo-version-check.sh
+++ t/t8902-silo-version-check.sh
@@ -14,12 +14,26 @@ echo git version 1.7.0
 EOFTXT
 chmod a+x git
 
-test_expect_success "git silo should fail if git version too low." '
+test_expect_success "setup" '
     git_silo="$(locate_git_silo)" &&
     git init &&
-    export PATH=.:$PATH &&
+    export PATH=.:$PATH
+'
+
+test_expect_success "git silo should fail if git version too low." '
     ! "${git_silo}" init 2>errmsg &&
     grep -q "version too low" errmsg
+'
+
+cat >git <<EOFTXT
+#!/bin/bash
+
+echo git version 2.0.0
+EOFTXT
+chmod a+x git
+
+test_expect_success "git silo accept git version 2.x." '
+    "${git_silo}" init
 '
 
 test_done
