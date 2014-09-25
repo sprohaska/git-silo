994763f2f28bed4121791f9234c212866522ba83
Steffen Prohaska <prohaska@zib.de>
2014-09-25 20:55:32 +0200

    add --attr, --no-attr to control whether gitattributes are modified
    The wildcard setup has proved to be more robust in practice than the
    automatic gitattributes handling.  When using wildcards, files can be
    freely renamed as long as the extension is kept.  With explicit
    gitattributes, renaming is problematic, because the gitattributes
    usually are forgotton.
    
    This introduced --no-attr to tell 'git add' to skip files if the
    gitattributes are not already set.  The default is kept unchanged.

--- t/t8911-silo-add-attributes.sh
+++ t/t8911-silo-add-attributes.sh
@@ -12,13 +12,39 @@ test_expect_success "setup user" '
 '
 
 test_expect_success \
-"git add should not create .gitattributes if wildcard attributes are ok." '(
+"'add' does not create .gitattributes if wildcard attributes are ok." '(
     cd wildcard &&
     echo "*.ext -text filter=silo" >.gitattributes &&
     mkdir subdir &&
     touch "subdir/a a.ext" &&
     git silo add "subdir/a a.ext" &&
     ! [ -e subdir/.gitattributes ]
+)'
+
+test_expect_success \
+"'add --no-attr' does not add file without filter=silo attribute but exits with success." '(
+    cd wildcard &&
+    touch "subdir/b b" &&
+    git silo add --no-attr "subdir/b b" 2>err &&
+    ( git status --porcelain -- "subdir/b b" | grep "^??" ) &&
+    grep "missing.*filter=silo" err
+)'
+
+test_expect_success \
+"'add --attr' adds file and modifies gitattributes." '(
+    cd wildcard &&
+    touch "subdir/b b" &&
+    git silo add --attr "subdir/b b" 2>err &&
+    ( git status --porcelain -- "subdir/b b" | grep "^A" ) &&
+    [ -e subdir/.gitattributes ]
+)'
+
+test_expect_success \
+"'add --no-attr' adds file with filter=silo attribute." '(
+    cd wildcard &&
+    touch "subdir/c c.ext" &&
+    git silo add --no-attr "subdir/c c.ext" 2>err &&
+    ( git status --porcelain -- "subdir/c c.ext" | grep ^A )
 )'
 
 test_done
