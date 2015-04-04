ae590231a07a49232ef8e3ac0b160427a72ef24b
Steffen Prohaska <prohaska@zib.de>
2015-04-04 16:33:21 +0200

    t8910-silo-add: refactor filename tests to function

--- t/t8910-silo-add.sh
+++ t/t8910-silo-add.sh
@@ -12,54 +12,62 @@ test_expect_success "setup user" '
     setup_user
 '
 
-test_expect_success "'git silo add' handles paths with spaces." '
-    setup_repo spaces && (
+testFilename() {
+    repo="$1"
+    filename="$2"
+
+    test_expect_success "'git silo add' handles paths '${filename}'." '
+        setup_repo ${repo} && (
+            cd ${repo} &&
+            echo a >"${filename}" &&
+            git silo add --attr "${filename}" &&
+            git commit -m "Add a a" &&
+            ( test $(blobSize "${filename}") -eq 41 ||
+                ( echo "Wrong blob size." && false ) )
+        )
+    '
+
+    test_expect_success \
+    "'git checkout' of silo content handles paths '${filename}'." '(
+        cd ${repo} &&
+        rm "${filename}" &&
+        git checkout "${filename}" &&
+        test -e "${filename}"
+    )'
+
+    test_expect_success "'git silo checkout' handles paths '${filename}'." '(
+        cd ${repo} &&
+        rm "${filename}" &&
+        git silo checkout "${filename}" &&
+        test -e "${filename}"
+    )'
+
+    test_expect_success "'add' updates committed files." '(
+        cd ${repo} &&
+        git silo checkout --copy "${filename}" &&
+        echo a >>"${filename}" &&
+        git silo add -- "${filename}" &&
+        ( git status --porcelain -- "${filename}" | grep ^M )
+    )'
+
+    test_expect_success "'add' remove file." '(
+        cd ${repo} &&
+        rm "${filename}" &&
+        git silo add -- "${filename}" 2>err &&
+        touch empty &&
+        test_cmp empty err &&
+        ( git status --porcelain | grep -q "^D  .${filename}." )
+    )'
+
+    test_expect_success "'add' handles deleted file twice." '(
         cd spaces &&
-        echo a >"a a" &&
-        git silo add --attr "a a" &&
-        git commit -m "Add a a" &&
-        ( test $(blobSize "a a") -eq 41 ||
-            ( echo "Wrong blob size." && false ) )
-    )
-'
+        git silo add -- "${filename}" 2>err &&
+        test_cmp empty err
+    )'
+}
 
-test_expect_success \
-"'git checkout' of silo content handles paths with spaces." '(
-    cd spaces &&
-    rm "a a" &&
-    git checkout "a a" &&
-    test -e "a a"
-)'
-
-test_expect_success "'git silo checkout' handles paths with spaces." '(
-    cd spaces &&
-    rm "a a" &&
-    git silo checkout "a a" &&
-    test -e "a a"
-)'
-
-test_expect_success "'add' updates committed files." '(
-    cd spaces &&
-    git silo checkout --copy "a a" &&
-    echo a >>"a a" &&
-    git silo add -- "a a" &&
-    ( git status --porcelain -- "a a" | grep ^M )
-)'
-
-test_expect_success "'add' remove file." '(
-    cd spaces &&
-    rm "a a" &&
-    git silo add -- "a a" 2>err &&
-    touch empty &&
-    test_cmp empty err &&
-    ( git status --porcelain | grep -q "^D  .a a." )
-)'
-
-test_expect_success "'add' handles deleted file twice." '(
-    cd spaces &&
-    git silo add -- "a a" 2>err &&
-    test_cmp empty err
-)'
+testFilename 'spaces' 'a a'
+# testFilename 'uml' 'ä ö ü'
 
 # Create multiple symlinks to ensure that lsSiloTracked() sees multiple
 # entries.  Use newline in the first symlink so that it closely resembles a
